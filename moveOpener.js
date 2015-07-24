34fbab29b997175e41d849dccc4fbc7998a05aa2
lukewoodSMU <lswood@smu.edu>
2015-07-24 12:37:49 -0700

    Added Stuff

---
+++ moveOpener.js
@@ -0,0 +1,14 @@
+<SCRIPT LANGUAGE="JavaScript">
+function moveOff(divName, i)
+{
+	document.getElementById(divName).className += ' offscreen';
+	if(i ==1)
+	{
+		alert("meow");
+	}
+	else
+	{
+		alert("moo");
+	}
+}
+</SCRIPT>
