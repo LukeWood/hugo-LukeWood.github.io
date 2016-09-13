0151816a03375212ae13411797b6dcbaa9df3efc
Luke Wood <lswood@smu.edu>
2016-09-13 13:43:53 -0500

    mobile beta

--- react/index.js
+++ react/index.js
@@ -27,6 +27,30 @@ var Title = React.createClass({displayName:"Title",
 });
 function renderMobile()
 {
+	var butcont = document.getElementById("buttoncontainer");
+	var txt = document.getElementById("menutext"); 
+	var menu = document.getElementById("menuButton");
+	txt.className = "mobilemenutext";
+	txt.style.position = "relative";
+	txt.style.display="inline-block";
+	menu.style.display="inline-block";
+	txt.style.marginLeft="0";
+	menu.style.marginRight="0";
+	
+	$("#buttoncontainer").click(function(){
+	Menu.activateMenu();
+	if($("#menu").is(":visible"))
+	{$("#menu").slideUp(650);$("#cover").slideUp(650)}
+	else{$("#menu").slideDown(650);$("#cover").slideDown(650)}});
+	butcont.setAttribute("style","width:100%;cursor:pointer");
+	document.getElementById("menu").style.width="100%";
+	var ctr = document.createElement("center");
+	ctr.style.overflow="visible";
+	ctr.appendChild(menu);
+	ctr.appendChild(txt);
+	
+	butcont.appendChild(ctr);
+	
 	$.ajax({
 		dataType:"json",
 		url:"projects.json",
