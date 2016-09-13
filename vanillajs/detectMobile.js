43c534462e6ef699a5b5566705732fed2a07262c
Luke Wood <lswood@smu.edu>
2016-09-13 13:23:12 -0500

    detectmobile

--- vanillajs/detectMobile.js
+++ vanillajs/detectMobile.js
@@ -1,10 +1,1 @@
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-	window.mobile = true;
-	console.log("Rendering mobile version");
-}
-else
-{
-	window.mobile = false;
-	console.log("Desktop version will be rendered");
-}
 
