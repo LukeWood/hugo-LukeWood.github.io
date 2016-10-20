cef9b582a635ed767911b0c03f9c2b2bd5c26c5c
Luke Wood <lswood@smu.edu>
2016-10-20 13:19:22 -0500

    Yay, less collisions

--- react/ProjectList.js
+++ react/ProjectList.js
@@ -1,9 +1,18 @@
 var currentLocs = [];
-
+var to_remove = [];
 
 function genNextPoint()
 {
+		var clone = JSON.parse(JSON.stringify(to_remove));
+		to_remove = [];
+		clone.forEach(function(rem){
+				for(var i = 0; i < currentLocs.length; i++){
+						if(currentLocs[i].x == rem.x && currentLocs[i].y == rem.y){
 
+						}
+						currentLocs.splice(i,1);
+				}
+		});
 		var best_pt = {x: Math.floor(Math.random() * window.innerWidth)-50,
 		y: 350 + Math.floor(Math.random() *(window.innerHeight-400))};
 
@@ -19,7 +28,7 @@ function genNextPoint()
 			{
 					var t_dist = Math.pow(pt.x - currentLocs[i].x,2) + Math.pow(pt.y - currentLocs[i].y,2);
 					if(t_dist < min_dist){
-							t_dist = min_dist;
+						  min_dist = min_dist;
 					}
 			}
 			if(min_dist < best_dist){
@@ -28,17 +37,17 @@ function genNextPoint()
 			}
 		}
 
-		for(var i = 0; i <  4; i++){
-			for(var j = 0; j < 4; j++){
-				var pt={x: Math.floor(i/10 * window.innerWidth)-50,
-				y: 350 + Math.floor(j/10 *(window.innerHeight-400))};
+		for(var i = 0; i <  5; i++){
+			for(var j = 0; j < 5; j++){
+				var pt={x: Math.floor(i/5 * window.innerWidth)-50,
+				y: 350 + Math.floor(j/5 *(window.innerHeight-400))};
 
 				var min_dist = 100000;
 				for(var c = 0; c < currentLocs.length; c++)
 				{
 						var t_dist = Math.pow(pt.x - currentLocs[c].x,2) + Math.pow(pt.y - currentLocs[c].y,2);
 						if(t_dist < min_dist){
-								t_dist = min_dist;
+								min_dist = min_dist;
 						}
 				}
 				if(min_dist < best_dist){
@@ -91,17 +100,7 @@ var Project = React.createClass({
 			if(opac<=0){
 				clearInterval(this.timer);
 				this.timer = setInterval(this.tick,Math.floor(Math.random() * 125));
-				var index = -1;
-			  for(var i = 0; i < currentLocs.length; i++)
-				{
-						if(currentLocs[i].x == this.state.x && currentLocs[i].y == this.state.y)
-						{
-							index = i;
-						}
-				}
-				if(index != -1){
-					currentLocs.splice(index,1);
-				}
+				to_remove.push({x:this.state.x, y:this.state.y});
 				var pt = genNextPoint();
 				this.setState({x:pt.x,
 						y:pt.y,
