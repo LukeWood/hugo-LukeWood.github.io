f5ed2fb454e221d8d4b0c961529136deecd60db2
Luke Wood <lukewoodcs@gmail.com>
2019-11-04 22:23:03 -0800

    Site rebuild Mon Nov  4 22:23:02 PST 2019

--- public/sw.js
+++ public/sw.js
@@ -14,15 +14,12 @@ this.addEventListener('install', event => {
   )
 })
 
-this.addEventListener('fetch', event => {
-  // request.mode = navigate isn't supported in all browsers
-  // so include a check for Accept: text/html header.
-  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-        event.respondWith(
-          fetch(event.request).catch(error => {
-              // Return the offline page
-              return caches.match('/offline');
-          })
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate' || (event.request.method === 'GET') && (event.request.headers.get('accept').includes('text/html'))) {
+    event.respondWith(
+      fetch(event.request).catch(error => {
+        return caches.match("/offline");
+      })
     );
   }
 });
