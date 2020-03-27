38b660f809e4c2b87cb7fbf86d774da38cd41a3f
Luke Wood <lukewoodcs@gmail.com>
2020-03-26 22:21:05 -0700

    Site rebuild Thu Mar 26 22:21:04 PDT 2020

--- js/darkmode.min.7d7341c61e13b0b7660636875a1d5ecb3f169ae545fc46104e82ec5302464530.js
+++
@@ -1,4 +0,0 @@
-if(localStorage.getItem("preferredTheme")===null){var theme=(window.matchMedia&&window.matchMedia("(prefers-color-scheme: light)").matches)?"light":"dark"
-localStorage.setItem("preferredTheme",theme);}
-function getTheme(){return window.localStorage.getItem("preferredTheme")||"dark";}
-document.documentElement.setAttribute("data-theme",getTheme());
--- js/preload.min.63d20478881c7ac6a2bd1867d8d7ea40d73de863b0a9e88784c255e981ad55ea.js
+++
@@ -1,1 +0,0 @@
-window.addEventListener('load',function(){document.body.classList.remove("preload")});
